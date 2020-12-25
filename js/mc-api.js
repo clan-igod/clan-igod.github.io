9e887f490ffc9b63f8758380714f80d35e867085
clan-igod <nachohdgamingpro02@gmail.com>
2020-12-25 18:56:42 +0100

    añadido misceáneo.html y modificaciones en el resto de -html's

--- js/mc-api.js
+++ js/mc-api.js
@@ -4,8 +4,8 @@ fetch("https://eu.mc-api.net/v3/server/ping/notmrmario.aternos.me")
 })
 
 .then(function (json) {
-    document.querySelector('#stus').innerHTML = json.online ? 'Online' : 'Offline';
-        document.querySelector("#nply").innerHTML = json.online ? 'Jugadores: ' + `<b>${json.players.online}</b>` + '/<b>50</b>' : 'Server Offline';
+    document.querySelector('#stus').innerHTML = json.online ? '<b draggable="true">Estado:</b> Online' : '<b draggable="true">Estado:</b> Offline';
+        document.querySelector("#nply").innerHTML = json.online ? `<b draggable="true">Número de jugadores: </b><b>${json.players.online}</b>` + ` <xd draggable="true">de</xd> <b>${json.players.max}</b>` : '<b draggable="true">Número de jugadores:</b> Server Offline';
         document.querySelector('#motd').innerHTML = json.online ? json.description.text.replace(/\§4/gi, ' ')
         .replace(/\§a/gi, ' ')
         .replace(/\§d/gi, ' ')
@@ -15,5 +15,8 @@ fetch("https://eu.mc-api.net/v3/server/ping/notmrmario.aternos.me")
         .replace(/\§6/gi, ' ')
         .replace(/\§7/gi, ' ')
         .replace(/\§8/gi, ' ') : 'Servidor offline';
+        document.querySelector('#version').innerHTML = json.online ? '<b draggable="true">Versión: </b>' + json.version.name
+        .replace(/\§4/, ' ')
+        .replace(/\● /, ' ') : '<b draggable="true">Versión:</b> Servidor offline';
         
 })
